f2b6fd44c59a1539bc4dca96c7eea4a27565b3e1
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-05-29 16:03:04 +0700

    443

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT;
 const cors = require("cors");
 const router = require("./src/routes/helloRouter");
