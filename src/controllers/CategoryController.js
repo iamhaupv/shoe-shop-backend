911655f9d44df46dc4f8a307963d9a87a3594c60
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-14 16:41:32 +0700

    update createCategory

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,8 +1,8 @@
 const { CategoryRepository } = require("../repositories/index");
 const createCategory = async (req, res) => {
   try {
-    const { name } = req.body;
-    const category = await CategoryRepository.createCategory(name);
+    const { name, description } = req.body;
+    const category = await CategoryRepository.createCategory(name, description);
     res.status(201).json({
       message: "Create category successfully!",
       data: category,
--- src/repositories/CategoryRepository.js
+++ src/repositories/CategoryRepository.js
@@ -1,13 +1,16 @@
 const { Category } = require("../models/index");
 // create category
-const createCategory = async (name) => {
+const createCategory = async (name, description) => {
   try {
     const categoryExist = await Category.findOne({ name });
+    const updatedAt = null;
     if (categoryExist) {
       throw new Error("Name is exist!");
     }
     const category = await Category.create({
       name,
+      description,
+      updatedAt
     });
     return category;
   } catch (error) {
