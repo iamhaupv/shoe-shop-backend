32c24f0c1524c035a29f11ba97b5d5f5be0ff2c5
longsoisuaxe1a <phamvanhau692002vip@gmail.com>
2024-06-15 18:14:03 +0700

    update models/Product

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,23 +2,43 @@ const mongoose = require("mongoose");
 
 const Product = mongoose.model(
   "Product",
-  new mongoose.Schema({
-    id: {
-      type: mongoose.ObjectId,
+  new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: false,
+      },
+      color: {
+        type: String,
+        required: false
+      },
+      material: {
+        type: String,
+        required: false
+      },
+      design: {
+        type: String,
+        required: false
+      }
     },
-    name: {
-      type: String,
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    category: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Category",
-    },
-  })
+    { timestamps: true }
+  )
 );
 
 module.exports = Product;
